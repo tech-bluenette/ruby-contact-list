34601a739d73aa1f8491542c06154fd677b32af9
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-20 22:33:00 +0000

    initial commit

---
+++ application.rb
@@ -0,0 +1,23 @@
+class Application
+ 
+  def initialize
+    # Start with an empty array of contacts.
+    # TODO: Perhaps stub (seed) some contacts so we don't have to create some every time we restart the app
+    @contacts = []
+  end
+ 
+  def run
+    show_main_menu
+    input = gets.chomp
+  end
+  
+  # Prints the main menu only
+  def show_main_menu
+    puts "Welcome to the app. What's next?"
+    puts " new      - Create a new contact"
+    puts " list     - List all contacts"
+    puts " show :id - Display contact details"
+    print "> "
+  end
+ 
+end
---
+++ contact.rb
@@ -0,0 +1,15 @@
+class Contact
+  
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :email
+  
+  def initialize(name, email)
+    # TODO: assign local variables to instance variables
+  end
+  
+  def to_s
+    # TODO: return string representation of Contact
+  end
+  
+end
